1fdb67cd2d90aa766f1d838626f36b99f71e3f8f
keyongyu <247321453@qq.com>
2015-09-02 12:31:02 +0800

    ...

--- YGOClient/CreateRoomForm.cs
+++ YGOClient/CreateRoomForm.cs
@@ -1,27 +1,25 @@
 ﻿/*
  * 由SharpDevelop创建。
  * 用户： Administrator
- * 日期: 2015/9/1
- * 时间: 18:48
+ * 日期: 2015/9/2
+ * 时间: 12:29
  * 
  * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
  */
 using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace YGOClient
 {
 	/// <summary>
-	/// Description of RoomTool.
+	/// Description of CreateRoomForm.
 	/// </summary>
-	public class RoomTool
+	public partial class CreateRoomForm : Form
 	{
-		
-		public static void CreateRoom(string cmd)
+		public CreateRoomForm()
 		{
-			
-		}
-		public static void JoinRoom(string cmd)
-		{
+			InitializeComponent();
 		}
 	}
 }
