cd90c2b86f067f4e38acec3539b48f19a1181cc0
keyongyu <247321453@qq.com>
2015-11-03 18:07:05 +0800

    fix json

---
+++ YGOClient/Common/GameRoom.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： Administrator
+ * 日期: 2015/11/3
+ * 时间: 17:55
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+
+namespace YGOCore.Game
+{
+	/// <summary>
+	/// Description of GameRoom.
+	/// </summary>
+	public class GameRoom
+	{
+		public GameRoom()
+		{
+		}
+	}
+}
