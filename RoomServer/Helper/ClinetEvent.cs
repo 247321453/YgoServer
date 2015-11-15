2e1ef2abfbcf94b2f442cbeebec2dcf9bc2a54f5
keyongyu <247321453@qq.com>
2015-11-15 18:16:04 +0800

    重复登陆

--- RoomServer/Helper/ClinetEvent.cs
+++ RoomServer/Helper/ClinetEvent.cs
@@ -76,15 +76,14 @@ namespace YGOCore
 				//返回聊天端口，对战端口
 				if(session.Server!=null){
 					session.Server.OnSendServerInfo(session);
+					session.Server.server_OnPlayerJoin(session.ServerInfo, session.Name, null);
 					lock(session.Server.Clients){
 						if(session.Server.Clients.ContainsKey(session.Name)){
-							session.Server.Clients[session.Name].Close();
-							session.Server.Clients[session.Name] = session;
+							session.SendError("已经登录");
 						}else{
 							session.Server.Clients.Add(session.Name, session);
 						}
 					}
-					session.Server.server_OnPlayerJoin(session.ServerInfo, session.Name, null);
 				}
 			}else{
 				session.SendError("认证失败");
